9d511f10d79a7318f14497bcb7077fd26862ce59
Layla Paula <layhpaula@gmail.com>
2024-03-12 10:15:05 -0300

    fix: crud de usuarios

--- app/Models/User.php
+++ app/Models/User.php
@@ -34,8 +34,8 @@ class User extends Authenticatable
     {
         $produto = $this->where(function ($query) use ($search) {
             if ($search) {
-                $query->where('nome', $search);
-                $query->orWhere('nome', 'LIKE', "%{$search}%");
+                $query->where('name', $search);
+                $query->orWhere('name', 'LIKE', "%{$search}%");
             }
         })->get();
 
